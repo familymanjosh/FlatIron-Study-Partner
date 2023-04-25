789a1e1ec86dfa79bc715815ff06452eb2d1024c
+ <familymanjosh@gmail.com>
2023-04-25 16:02:05 -0700

    fix json

--- study-partner/src/Components/LectureCard.js
+++ study-partner/src/Components/LectureCard.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 const LectureCard = ({ lecture }) => {
-  const { title, image, instructor, description, link, notes, topic } = lecture;
+  const { title, link, instructor, notes } = lecture;
 
   return (
     <div className="card">
       <card>
-        <h2>{topic}</h2>
+        <h2>{title}</h2>
         <p>{instructor}</p>
         <p>{notes}</p>
         <iframe
           width="560"
           height="315"
           src={link}
-          title={topic}
+          title={title}
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
